646cc32eea7d754d2702be60098392bbd4e88ca7
SaltedFish <>
2025-07-22 12:55:05 +0800

    第一次提交

---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+import jooq.database.postgres.tables.pojos.漫画BO;
+import jooq.database.postgres.tables.漫画TB;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+public class Main {
+
+    public static void main(String[] args) {
+        // 数据库连接信息
+        String url = "jdbc:postgresql://localhost:5432/service"; // 替换为你的数据库名
+        String user = "postgres"; // 替换为你的数据库用户名
+        String password = "123456"; // 替换为你的数据库密码
+
+        Connection connection = setConnection(url, user, password);
+        // 获取 jOOQ 执行器
+        /*DSLContext dslContext = DSL.using(connection, SQLDialect.POSTGRES);
+        List<漫画BO> list = dslContext.select(漫画TB.漫画).from(漫画TB.漫画).orderBy(漫画TB.漫画.ID).fetchInto(漫画BO.class);
+        list.forEach(System.out::println);*/
+    }
+
+
+    public static Connection setConnection(String url, String user, String password) {
+        try {
+            // 1. 加载 JDBC 驱动
+            Class.forName("org.postgresql.Driver");
+            // 2. 连接数据库
+            return DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
